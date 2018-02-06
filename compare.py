622b127068070fd57ce9426409f8ac0a67c9cf9d
venkys10 <s.venkatesh779@gmail.com>
2018-02-06 15:53:56 -0600

    question 2 working

--- compare.py
+++ compare.py
@@ -3,22 +3,25 @@
 from sys import argv
 
 import argparse
-'''
-first, second, third = argv
 
-print "first:", first
-print "second:", second
-print "third:", third
-'''
+first, second, third = argv
 def number_of_words(second, third):
-    first, second = argv
     num_words = 0
     with open(second, 'r') as wap:
         for line in wap:
             words = line.split()
             num_words = num_words + len(words)
-
+        print "name of book", second
         print "number of words:", num_words
 
-if '__name__'==
+    with open(third, 'r') as new:
+        for line in new:
+            words = line.split()
+            num_words = num_words + len(words)
+        print "name of book", third
+        print "number of words:", num_words
+
+if __name__ == '__main__':
+
+    number_of_words(second, third)
 #test_words('foo', 'bar')
